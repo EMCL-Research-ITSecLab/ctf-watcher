22dc6b036c346cdd6713d280f5bc2a841196973c
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-12 11:48:21 +0100

    Update ufw_set_up.sh

--- WazuhDocker/config/ufw_set_up.sh
+++ WazuhDocker/config/ufw_set_up.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+ufw enable
+
+ufw allow 22/tcp
+ufw allow 9200/tcp
+ufw allow 443/tcp
+ufw allow 1514/tcp
+ufw allow 1514/udp
+ufw allow 1515/tcp
+ufw allow 7000/tcp
+ufw allow 7000/udp
