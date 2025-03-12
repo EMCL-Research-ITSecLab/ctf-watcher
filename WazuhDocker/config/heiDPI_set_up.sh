b6ce935269b599fe316c618a62215facef43ba0a
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-12 12:15:31 +0100

    Update heiDPI_set_up.sh to curl newest Ggolocation data

--- WazuhDocker/config/heiDPI_set_up.sh
+++ WazuhDocker/config/heiDPI_set_up.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+ABSOLUTE_PATH_CONFIG=$(realpath heiDPI_config.yml)
+
+curl -O https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
+gunzip GeoLite2-City.mmdb.gz
+mv GeoLite2-City.mmdb /tmp/city.mmdb 
+
 sudo rm -f /var/lib/docker/volumes/heiDPI_tmp/_data/nDPIsrvd-daemon-distributor.sock
 sudo rm -f /var/lib/docker/volumes/heiDPI_tmp/_data/nDPIsrvd-daemon-collector.sock
 
@@ -11,7 +17,6 @@ sudo docker run --name heiDPI_consumer \
 -e SHOW_ERROR_EVENTS=1 -e SHOW_DAEMON_EVENTS=1 -e SHOW_PACKET_EVENTS=1 -e SHOW_FLOW_EVENTS=1 \
 --mount type=volume,src=heiDPI_var_log,dst=/var/log \
 --mount type=volume,src=heiDPI_tmp,dst=/tmp \
---mount type=bind,src=/home/fdaas/heiDPI_config.yml,dst=/usr/src/app/config.yml \
+--mount type=bind,src="$ABSOLUTE_PATH_CONFIG",dst=/usr/src/app/config.yml \
 --mount type=bind,src=/tmp/city.mmdb,dst=/tmp/city.mmdb \
 -d -e HOST=127.0.0.1 --net host stefan96/heidpi-consumer:main
-
