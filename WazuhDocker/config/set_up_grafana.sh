3528e3fe54c77f6013d8ff3a5f5abacc1e49a99f
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-12 17:12:01 +0100

    Add update uid in wazuh_dashboard.json  inset_up_grafana.sh

--- WazuhDocker/config/set_up_grafana.sh
+++ WazuhDocker/config/set_up_grafana.sh
@@ -10,6 +10,10 @@ curl -X POST "$GRAFANA_URL/api/datasources" \
   -H "Content-Type: application/json" \
   -d @$DATASOURCE_JSON
 
+##Get The uid Grafana assigned to the new datasource and replace the old uid in DASHBOARD_JSON so the Dashboard is connected to the datasource  
+DATASOURCE_UID=(curl -X GET "http://localhost:3000/api/datasources/1" -u admin:admin 2>/dev/null | grep -o '"uid":"[^"]*' | sed 's/"uid":"//')
+sed 's/"uid": "\${DS_WAZUH-2}"/"uid": "'$DATASOURCE_UID'"/' $DASHBOARD_JSON > temp.json && mv temp.json $DASHBOARD_JSON
+
 curl -v -X POST "$GRAFANA_URL/api/dashboards/db" \
   -u "$GRAFANA_USERNAME:$GRAFANA_PASSWORD" \
   -H "Content-Type: application/json" \
