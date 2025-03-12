ea84da5f2ded3d5935567f46c68e9018f579250a
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-12 16:24:32 +0100

    Upload datasource before dashboard inset_up_grafana.sh

--- WazuhDocker/config/set_up_grafana.sh
+++ WazuhDocker/config/set_up_grafana.sh
@@ -5,13 +5,14 @@ GRAFANA_USERNAME="admin"
 DASHBOARD_JSON="wazuh_dashboard.json"
 DATASOURCE_JSON="wazuh_datasource.json"
 
-
-#curl -v -X POST "$GRAFANA_URL/api/dashboards/db" \
-#  -u "$GRAFANA_USERNAME:$GRAFANA_PASSWORD" \
-#  -H "Content-Type: application/json" \
-#  -d @$DASHBOARD_JSON
-
 curl -X POST "$GRAFANA_URL/api/datasources" \
   -u "$GRAFANA_USERNAME:$GRAFANA_PASSWORD" \
   -H "Content-Type: application/json" \
   -d @$DATASOURCE_JSON
+
+curl -v -X POST "$GRAFANA_URL/api/dashboards/db" \
+  -u "$GRAFANA_USERNAME:$GRAFANA_PASSWORD" \
+  -H "Content-Type: application/json" \
+  -d @$DASHBOARD_JSON
+
+
