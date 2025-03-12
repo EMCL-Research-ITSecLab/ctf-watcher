97116fda4ac0ff848202e15cf1917cae43c3a98d
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-12 16:24:00 +0100

    Remove whitespace in set_up_grafana.sh

--- WazuhDocker/config/set_up_grafana.sh
+++ WazuhDocker/config/set_up_grafana.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 GRAFANA_URL="http://localhost:3000"
-GRAFANA_USERNAME="admin"
 GRAFANA_PASSWORD="admin"
+GRAFANA_USERNAME="admin"
 DASHBOARD_JSON="wazuh_dashboard.json"
 DATASOURCE_JSON="wazuh_datasource.json"
 
 
-curl -v -X POST "$GRAFANA_URL/api/dashboards/db" \
+#curl -v -X POST "$GRAFANA_URL/api/dashboards/db" \
+#  -u "$GRAFANA_USERNAME:$GRAFANA_PASSWORD" \
+#  -H "Content-Type: application/json" \
+#  -d @$DASHBOARD_JSON
+
+curl -X POST "$GRAFANA_URL/api/datasources" \
   -u "$GRAFANA_USERNAME:$GRAFANA_PASSWORD" \
   -H "Content-Type: application/json" \
-  -d @$DASHBOARD_JSON
-
-curl -X POST http://localhost:3000/api/datasources \
-  -u "$GRAFANA_USERNAME:$GRAFANA_PASSWORD" \ 
-  -H "Content-Type: application/json" \
-  -d @DATASOURCE_JSON
-
+  -d @$DATASOURCE_JSON
