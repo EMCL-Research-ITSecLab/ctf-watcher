fb3105bcc23ed8f1eb435f848b908cf6e6345bc1
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-19 16:18:27 +0100

    Add help message in set_up_agent.sh

--- WazuhAgent/set_up_agent.sh
+++ WazuhAgent/set_up_agent.sh
@@ -30,6 +30,34 @@ CMD_INSTALL="$CMD_INSTALL_DEB_AMD"
 CMD_RUN="$CMD_RUN_LINUX"
 OS="$OS_DEB_AMD"
 
+HELP="
+Usage: set_up_agent.sh [OPTIONS]
+
+    --manager=<ip_address>         [Optional] Set the Wazuh Manager ip address this Agent should report to.
+                                   [Default] = localhost
+
+    --use_bash_log=<true/false>    [Optional] Set if bash commands should be logged
+                                   [Default] = true
+
+    --use_heidpi=<true/false>      [Optional] Set if heiDPId should be installed, set up and logged
+                                   [Default] = true
+
+    --use_ufw=<true/false>         [Optional] Set if UFW should be set up and logged
+                                   [Default] = true
+
+    --os=<                         [Optional] Set the os the Agent should run on. Supportet os are:
+          rpm_amd/                     Linux RPM amd64
+          rpm_aarch/                   Linux RPM aarch64
+          deb_amd/                     Linux DEB amd64
+          deb_aarch/                   Linux DEB aarch64
+          win/                         Windows MSI 32/64 bits
+          mac_intel/                   macOS intel
+          mac_sillicon                 macOS Apple silicon
+          >
+
+                                   [Default] = Linux DEB amd64
+    -h, --help                     [Optional] Show this help."
+
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --manager=*)
@@ -78,6 +106,10 @@ while [[ $# -gt 0 ]]; do
       CMD_INSTALL="$CMD_INSTALL_MAC_SILLICON"
       CMD_RUN="$CMD_RUN_MAC"
       OS="$OS_SILLICON"
+      ;;
+    -h|--help)
+      echo "$HELP"
+      exit 0
       ;;
     *)
       echo "Unknown option: $1"
