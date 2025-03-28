201f20295382976972c00568519a8a14d38c96df
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-28 15:06:51 +0100

    Update set_up_manager.sh

--- WazuhDocker/set_up_manager.sh
+++ WazuhDocker/set_up_manager.sh
@@ -5,18 +5,6 @@ IP_ADDRESS=$(hostname -I | awk '{print $1}')
 RAM_MIN=8388608
 RAM=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
 
-cat << "EOF"
- __          __             _       _____           _        _ _           
- \ \        / /            | |     |_   _|         | |      | | |          
-  \ \  /\  / /_ _ _____   _| |__     | |  _ __  ___| |_ __ _| | | ___ _ __ 
-   \ \/  \/ / _` |_  / | | | '_ \    | | | '_ \/ __| __/ _` | | |/ _ \ '__|
-    \  /\  / (_| |/ /| |_| | | | |  _| |_| | | \__ \ || (_| | | |  __/ |   
-     \/  \/ \__,_/___|\__,_|_| |_| |_____|_| |_|___/\__\__,_|_|_|\___|_|   
-                                                                           
-                                                                         
-EOF
-
-
 if [ $(id -u) -ne 0 ]; then
     echo "Please run this script as root or using sudo!"
     exit 0
@@ -36,9 +24,29 @@ if [ $RAM -le $RAM_MIN ]; then
     fi
  fi
 
-echo
-echo --------[1/4]Set Map Count--------
-echo
+function print_info()
+{
+echo ""
+echo -e "\e[34m[Info]:\e[0m $1"
+echo ""
+}
+
+function print_warning()
+{
+echo ""
+echo -e "\e[33m[Info]:\e[0m $1"
+echo ""
+}
+
+function print_error()
+{
+echo ""
+echo -e "\e[31m[Info]:\e[0m $1"
+echo ""
+}
+
+#####################################################
+print_info "Wazuh Manager set up [1/6] Set Map Count"
 
 sysctl -w vm.max_map_count=262144
 
@@ -48,43 +56,35 @@ else
     echo "vm.max_map_count=262144" >> "$SYSCTL_CONF"
 fi
 
-echo
-echo --------[2/4]Build Images--------
-echo
+print_info "Wazuh Manager set up [2/6] Build Images"
 
 cd wazuh-docker
 git checkout v4.11.1
-sudo -u $SUDO_USER ./build-docker-images/build-images.sh -v 4.11.1
 
-echo
-echo --------[3/3]Generat Certs--------
-echo
+#####################################################
+print_info "Wazuh Manager set up [4/6] Generating Certifications"
+
 
 cd single-node
 sudo -u $SUDO_USER docker-compose -f generate-indexer-certs.yml run --rm generator
 
-echo
-echo --------[4/4]Compose Docker--------
-echo
+#####################################################
+print_info "Wazuh Manager set up [4/6] Docker Compose"
 
 
 sudo -u $SUDO_USER docker-compose up -d
 
-echo
-echo --------[5/5]Set up custom rules--------
-echo
+#####################################################
+print_info "Wazuh Manager set up [4/6] Set up custom rules"
 
 cd ..
 cd ..
 
 docker cp config/local_rules.xml $(docker ps -aqf "name=single-node-wazuh.manager-1"):/var/ossec/etc/rules/local_rules.xml
 docker cp config/local_decoder.xml $(docker ps -aqf "name=single-node-wazuh.manager-1"):/var/ossec/etc/decoders/local_decoder.xml
-#docker cp config/ossec.conf $(docker ps -aqf "name=single-node-wazuh.manager-1"):/var/ossec/etc/decoders/local_decoder.xm
 
-
-echo
-echo --------[6/6]Restart Manager--------
-echo
+#####################################################
+print_info "Wazuh Manager set up [6/6] Restart Manager"
 
 docker restart $(docker ps -aqf "name=single-node-wazuh.manager-1")
 
