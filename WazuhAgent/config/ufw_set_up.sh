611eb46098d0c26ad041d898256826ae1b4086a4
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-03-29 09:39:55 +0100

    Sort and add 8080 and 9090 in ufw_set_up.sh

--- WazuhAgent/config/ufw_set_up.sh
+++ WazuhAgent/config/ufw_set_up.sh
@@ -3,10 +3,21 @@
 ufw enable
 
 ufw allow 22/tcp
-ufw allow 9200/tcp
+
 ufw allow 443/tcp
+
 ufw allow 1514/tcp
+
 ufw allow 1514/udp
 ufw allow 1515/tcp
+
 ufw allow 7000/tcp
 ufw allow 7000/udp
+
+ufw allow 8080/tcp
+ufw allow 8080/udp
+
+ufw allow 9090/tcp
+ufw allow 9090/udp
+
+ufw allow 9200/tcp
