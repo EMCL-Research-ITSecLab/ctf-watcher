3ef3e21675fe4aa29a458381d9e226ae8d6d55d1
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-04-19 17:11:11 +0200

    Add automatic restart in heiDPI_set_up.sh

--- WazuhAgent/config/heiDPI_set_up.sh
+++ WazuhAgent/config/heiDPI_set_up.sh
@@ -3,20 +3,18 @@ ABSOLUTE_PATH_CONFIG=$(realpath heiDPI_config.yml)
 
 curl -O https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
 gunzip GeoLite2-City.mmdb.gz
-mv GeoLite2-City.mmdb /tmp/city.mmdb 
-
-sudo rm -f /var/lib/docker/volumes/heiDPI_tmp/_data/nDPIsrvd-daemon-distributor.sock
-sudo rm -f /var/lib/docker/volumes/heiDPI_tmp/_data/nDPIsrvd-daemon-collector.sock
+mkdir -p /var/lib/GeoLite2/
+mv GeoLite2-City.mmdb /var/lib/GeoLite2/city.mmdb
 
 sudo docker run --name heiDPI_producer \
 --mount type=volume,src=heiDPI_var_log,dst=/var/log \
---mount type=volume,src=heiDPI_tmp,dst=/tmp \
+--restart unless-stopped \
 -d -p 127.0.0.1:7000:7000 --net host stefan96/heidpi-producer:main
 
 sudo docker run --name heiDPI_consumer \
 -e SHOW_ERROR_EVENTS=1 -e SHOW_DAEMON_EVENTS=1 -e SHOW_PACKET_EVENTS=1 -e SHOW_FLOW_EVENTS=1 \
 --mount type=volume,src=heiDPI_var_log,dst=/var/log \
---mount type=volume,src=heiDPI_tmp,dst=/tmp \
 --mount type=bind,src="$ABSOLUTE_PATH_CONFIG",dst=/usr/src/app/config.yml \
---mount type=bind,src=/tmp/city.mmdb,dst=/tmp/city.mmdb \
+--mount type=bind,src=/var/lib/GeoLite2/city.mmdb,dst=/var/lib/GeoLite2/city.mmdb \
+--restart unless-stopped \
 -d -e HOST=127.0.0.1 --net host stefan96/heidpi-consumer:main
