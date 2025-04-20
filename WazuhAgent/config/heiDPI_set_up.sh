c3c01ea880f269acec0f3c6e12e5b3737a0afda6
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-04-20 12:24:54 +0200

    Change to docker compose in heiDPI_set_up.sh

--- WazuhAgent/config/heiDPI_set_up.sh
+++ WazuhAgent/config/heiDPI_set_up.sh
@@ -4,17 +4,8 @@ ABSOLUTE_PATH_CONFIG=$(realpath heiDPI_config.yml)
 curl -O https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
 gunzip GeoLite2-City.mmdb.gz
 mkdir -p /var/lib/GeoLite2/
-mv GeoLite2-City.mmdb /var/lib/GeoLite2/city.mmdb
+mv GeoLite2-City.mmdb /var/lib/GeoLite2/city.mmdb 
 
-sudo docker run --name heiDPI_producer \
---mount type=volume,src=heiDPI_var_log,dst=/var/log \
---restart unless-stopped \
--d -p 127.0.0.1:7000:7000 --net host stefan96/heidpi-producer:main
+sed -i "s|<config_absolute_path>|$ABSOLUTE_PATH_CONFIG|g" docker-compose.yaml
 
-sudo docker run --name heiDPI_consumer \
--e SHOW_ERROR_EVENTS=1 -e SHOW_DAEMON_EVENTS=1 -e SHOW_PACKET_EVENTS=1 -e SHOW_FLOW_EVENTS=1 \
---mount type=volume,src=heiDPI_var_log,dst=/var/log \
---mount type=bind,src="$ABSOLUTE_PATH_CONFIG",dst=/usr/src/app/config.yml \
---mount type=bind,src=/var/lib/GeoLite2/city.mmdb,dst=/var/lib/GeoLite2/city.mmdb \
---restart unless-stopped \
--d -e HOST=127.0.0.1 --net host stefan96/heidpi-consumer:main
+docker compose -p heidpi -f docker-compose.yaml up -d
