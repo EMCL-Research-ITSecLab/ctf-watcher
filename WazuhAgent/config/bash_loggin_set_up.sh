c425d329a91272722fa96cb53ec0fa8d9fde4862
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-05-24 13:56:21 +0200

    Add private log to bash_loggin_set_up.sh

--- WazuhAgent/config/bash_loggin_set_up.sh
+++ WazuhAgent/config/bash_loggin_set_up.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 GLOBAL_BASHRC="/etc/bash.bashrc"
-COMMAND_LOG_BASH="
-
 CONTAINER_NAME="${ENV_CONTAINER_NAME}"
+PATH_BASH_CONF="/etc/rsyslog.d/bash.conf"
+LOG_DESTINATION="local6.* /var/log/commands.log"
+PRIVAT_LOG=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --privat_log=*)
+      PRIVAT_LOG="${1#*=}"
+      ;;
+
 if [ -z "$CONTAINER_NAME" ]; then CONTAINER_NAME=$(hostname); fi
 
+COMMAND_LOG_BASH="
 # log last bash command to local6.debug which is located in /var/log/commands.log
 export PROMPT_COMMAND='RETRN_VAL=\$?;logger -t bash_commands -p local6.debug \"Container $CONTAINER_NAME User \$(whoami) @ \$(pwd) [$$]: \$(history 1 | sed \"s/^[ ]*[0-9]\+[ ]*//\" )\"'
 "
-PATH_BASH_CONF="/etc/rsyslog.d/bash.conf"
-LOG_DESTINATION="local6.* /var/log/commands.log"
 
 # Check if the GLOBAL_BASHRC file exists
 if [ -e "$GLOBAL_BASHRC" ]; then
@@ -26,5 +33,9 @@ echo "$COMMAND_LOG_BASH" >> "$GLOBAL_BASHRC"
 source "$GLOBAL_BASHRC"
 
 echo "$LOG_DESTINATION" >> "$PATH_BASH_CONF"
+
+if [ -n "$PRIVATE_LOG" ]; then
+  echo "local6.* $PRIVATE_LOG" >> "$PATH_BASH_CONF"
+fi
 
 systemctl restart rsyslog
