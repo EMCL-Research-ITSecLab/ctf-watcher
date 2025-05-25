e7684f280b645fe7ce8960a0eb8024041352c78d
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-05-25 11:37:01 +0200

    Fix typos container_logging_set_up.sh

--- WazuhAgent/config/container_logging_set_up.sh
+++ WazuhAgent/config/container_logging_set_up.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker ps -q | xarg -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent
-docker ps -q | xarg -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent
+docker ps -q | xargs -I {} docker exec -i {} mkdir wazuh-agent
 
-docker ps -q | xargs -I {} docker exec -i {} sh -c "/container_requirements_set_up.sh"
+docker ps -q | xargs -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent
+docker ps -q | xargs -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent
+
+docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/container_requirements_set_up.sh"
 docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/bash_loggin_set_up.sh"
