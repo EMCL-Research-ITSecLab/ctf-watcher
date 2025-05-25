d5021202475475f3cbb1b605482566c4b9bde84e
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-05-25 11:23:18 +0200

    Add copieng code into container in container_logging_set_up.sh

--- WazuhAgent/config/container_logging_set_up.sh
+++ WazuhAgent/config/container_logging_set_up.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker ps -q | xargs -I {} docker exec -w /wazuh-agent -i {} bash < config/container_requirements_set_up.sh
-docker ps -q | xargs -I {} docker exec -w /wazuh-agent -i {} bash < config/bash_loggin_set_up.sh
+docker ps -q | xarg -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent
+docker ps -q | xarg -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent
+
+docker ps -q | xargs -I {} docker exec -i {} sh -c "/container_requirements_set_up.sh"
+docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/bash_loggin_set_up.sh"
