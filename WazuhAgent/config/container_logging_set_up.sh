342fc16f97edb6cec9e20713d78a21692096fa3c
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-05-26 17:11:25 +0200

    Add prints in container_logging_set_up.sh

--- WazuhAgent/config/container_logging_set_up.sh
+++ WazuhAgent/config/container_logging_set_up.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+function print_info()
+{
+echo ""
+echo -e "\e[34m[Info]:\e[0m $1"
+echo ""
+}
+
+print_info "Create /wazuh-agent directory in containers [1/4]"
+
 docker ps -q | xargs -I {} docker exec -i {} mkdir wazuh-agent
+
+
+print_info "Copy set up scripts into containers [2/4]"
 
 docker ps -q | xargs -I {} docker cp config/container_requirements_set_up.sh {}:/wazuh-agent/
 docker ps -q | xargs -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent/
 
+print_info "Install requirements on containers [3/4]"
+
 docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/container_requirements_set_up.sh"
+
+
+print_info "Set up bash logging in containers [4/4]"
+
 docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/bash_loggin_set_up.sh"
