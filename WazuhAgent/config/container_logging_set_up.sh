e2973dc922da388151150c14fef868ad33498089
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-05-27 11:07:51 +0200

    Move flag to correct script container_logging_set_up.sh

--- WazuhAgent/config/container_logging_set_up.sh
+++ WazuhAgent/config/container_logging_set_up.sh
@@ -19,9 +19,9 @@ docker ps -q | xargs -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agen
 
 print_info "Install requirements on containers [3/4]"
 
-docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/container_requirements_set_up.sh --privat_log=/wazuh-agent/commands.log"
+docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/container_requirements_set_up.sh"
 
 
 print_info "Set up bash logging in containers [4/4]"
 
-docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/bash_loggin_set_up.sh"
+docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/bash_loggin_set_up.sh --privat_log=/wazuh-agent/commands.log"
