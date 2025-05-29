bb44acb98c976baee5a634a8cc7aacb5b0e46c4e
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-05-29 18:15:26 +0200

    Update logging in container_logging_set_up.sh

--- WazuhAgent/config/container_logging_set_up.sh
+++ WazuhAgent/config/container_logging_set_up.sh
@@ -3,21 +3,21 @@
 function print_info()
 {
 echo ""
-echo -e "\e[34m[Info]:\e[0m  $SET_UP_STEP_MAIN | $SET_UP_STEP_SUB | $1"
+echo -e "\e[34m[Info]:\e[0m  $SET_UP_STEP_MAIN: $SET_UP_STEP_SUB:\e[2m $1"
 echo ""
 }
 
-print_info "[1/4] Create /wazuh-agent directory in containers"
+print_info "Create /wazuh-agent directory in containers...  1/4"
 
 docker ps -q | xargs -I {} docker exec -i {} mkdir wazuh-agent
 
 
-print_info "[2/4] Copy set up scripts into containers"
+print_info "Copy set up scripts into containers...  2/4"
 
 docker ps -q | xargs -I {} docker cp config/container_requirements_set_up.sh {}:/wazuh-agent/
 docker ps -q | xargs -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent/
 
-print_info "[3/4] Install requirements on containers"
+print_info "Install requirements on containers...  3/4"
 
 for container_id in $(docker ps -q); do
   docker exec "$container_id" sh -c "/wazuh-agent/container_requirements_set_up.sh" &
@@ -27,6 +27,6 @@ for pid in "${pids[@]}"; do
   wait "$pid"
 done
 
-print_info "[4/4] Set up bash logging in containers"
+print_info "Set up bash logging in containers...  4/4"
 
 docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/bash_loggin_set_up.sh --privat_log=/wazuh-agent/commands.log"
