469d782764ea664bd2f85f64ef284dcd770a0fa6
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-05-29 18:42:11 +0200

    Only run set up for non-excluded images in container_logging_set_up.sh

--- WazuhAgent/config/container_logging_set_up.sh
+++ WazuhAgent/config/container_logging_set_up.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+EXCLUDED_IMAGES=("grafana" "prom" "gcr.io" "stefan96/heidpi" "wazuh")
+function is_excluded_image() {
+  for prefix in "${EXCLUDED_IMAGES[@]}"; do
+    if [[ "$1" == "$prefix"* ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
 function print_info()
 {
 echo ""
@@ -8,18 +17,33 @@ echo ""
 }
 
 print_info "Create /wazuh-agent directory in containers...  1/4"
+for container_id in $(docker ps -q); do
+  IMAGE_NAME=$(docker inspect --format='{{.Config.Image}}' "$container_id")
+  if is_excluded_image "$IMAGE_NAME"; then
+    continue
+  fi
+  docker exec "$container_id" sh -c "mkdir -p /wazuh-agent"
+done
 
-docker ps -q | xargs -I {} docker exec -i {} mkdir wazuh-agent
 
 
 print_info "Copy set up scripts into containers...  2/4"
-
-docker ps -q | xargs -I {} docker cp config/container_requirements_set_up.sh {}:/wazuh-agent/
-docker ps -q | xargs -I {} docker cp config/bash_loggin_set_up.sh {}:/wazuh-agent/
+for container_id in $(docker ps -q); do
+  IMAGE_NAME=$(docker inspect --format='{{.Config.Image}}' "$container_id")
+  if is_excluded_image "$IMAGE_NAME"; then
+    continue
+  fi
+  docker cp config/container_requirements_set_up.sh "$container_id":/wazuh-agent/
+  docker cp config/bash_loggin_set_up.sh "$container_id":/wazuh-agent/
+done
 
 print_info "Install requirements on containers...  3/4"
-
+pids=()
 for container_id in $(docker ps -q); do
+  IMAGE_NAME=$(docker inspect --format='{{.Config.Image}}' "$container_id")
+  if is_excluded_image "$IMAGE_NAME"; then
+    continue
+  fi
   docker exec "$container_id" sh -c "/wazuh-agent/container_requirements_set_up.sh" &
   pids+=($!)
 done
@@ -28,5 +52,10 @@ for pid in "${pids[@]}"; do
 done
 
 print_info "Set up bash logging in containers...  4/4"
-
-docker ps -q | xargs -I {} docker exec -i {} sh -c "/wazuh-agent/bash_loggin_set_up.sh --privat_log=/wazuh-agent/commands.log"
+for container_id in $(docker ps -q); do
+  IMAGE_NAME=$(docker inspect --format='{{.Config.Image}}' "$container_id")
+  if is_excluded_image "$IMAGE_NAME"; then
+    continue
+  fi
+  docker exec "$container_id" sh -c "/wazuh-agent/bash_loggin_set_up.sh --privat_log=/wazuh-agent/commands.log"
+done
