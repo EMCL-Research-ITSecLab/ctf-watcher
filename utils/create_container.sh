35bdf33c567fbefaecf542cebee8c241caedd513
FeDaas <uv223@ix.urz.uni-heidelberg.de>
2025-06-02 11:12:36 +0000

    Update create_container.sh

--- utils/create_container.sh
+++ utils/create_container.sh
@@ -2,11 +2,14 @@
 NUMBER_OF_CONTAINERS=$1
 
 for i in $(seq 1 $NUMBER_OF_CONTAINERS); do
-  container_name="my-engine_$i"
+  CONTAINER_NAME="my-engine_$i"
+  mkdir -p command_logs
+  touch "command_logs/cl_$CONTAINER_NAME"
+  ABSOLUTE_PATH="$(realpath command_logs/cl_$CONTAINER_NAME)"
   docker run \
-    --name "$container_name" \
-    -e ENV_CONTAINER_NAME="$container_name" \
+    --name "$CONTAINER_NAME" \
+    -e ENV_CONTAINER_NAME="$CONTAINER_NAME" \
     --mount type=bind,source=/var/log/commands.log,target=/var/log/commands.log \
-    --mount type=bind,source=~/command_logs/cl_1.log,target=/wazuh-agent/commands.log \
+    --mount type=bind,source="$ABSOLUTE_PATH",target=/wazuh-agent/commands.log \
     -d nginx
 done
