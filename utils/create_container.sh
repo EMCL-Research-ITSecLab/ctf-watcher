e9e72d40d65c9c318a102e756a015cb6af17298d
FeDaas <44971780+FeDaas@users.noreply.github.com>
2025-06-02 11:52:42 +0200

    Create create_container.sh

---
+++ utils/create_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NUMBER_OF_CONTAINERS=$1
+
+for i in $(seq 1 $NUMBER_OF_CONTAINERS); do
+  container_name="my-x$i"
+  docker run \
+    --name "$container_name" \
+    -e ENV_CONTAINER_NAME="$container_name" \
+    --mount type=bind,source=/var/log/commands.log,target=/var/log/commands.log \
+    --mount type=bind,source=./command_logs/cl_1.log,target=/wazuh-agent/commands.log \
+    -d nginx
+done
